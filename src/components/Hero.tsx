927ad6a5a7db1ce119b92af0c7b1c0b42d161f57
JonasFrellesen <jonasfpetersen@gmail.com>
2026-06-06 14:46:40 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
         <p className="eyebrow">Cand.Polyt (Civilingeniør)</p>
         <h1>Jonas Frellesen Petersen</h1>
         <h3>Velkommen til min portfølje</h3>
-        <img src={new URL('./Profilbillede.png', import.meta.url).href} alt="Profile Image" width="50%" height="50%" />
+        <img src={`${(import.meta as any).env.BASE_URL}Profilbillede.png`} alt="Profile Image" />
       </div>
     </section>
   );
